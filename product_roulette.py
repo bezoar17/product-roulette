712836441599c61d57134e05143e847c76509f3f
Vaibhav Kashyap <vaibhavbkashyap@gmail.com>
2017-02-14 14:50:08 +0530

    Update product_roulette.py

--- product_roulette.py
+++ product_roulette.py
@@ -26,7 +26,6 @@ logger.addHandler(handler)
 '''Logging setup'''
 
 ''' GLOBALS FOR MODULE '''
-
 current_set_l=None
 new_set_l=None
 current_set_d=None
@@ -51,7 +50,6 @@ model_value=None
 ''' GLOBALS FOR MODULE '''
 
 ''' FUNCTION DEFINITIONS START'''
-
 def showProduct():
 	"""
 	Brief: Call update on current_product and then show the name of current_product
@@ -68,20 +66,15 @@ def showProduct():
 	"""
 	global logger
 	global current_product,db,cursor,current_product_name,all_suggestions
-	logger.info('Fn: showProduct() called with valid_choice=%d',valid_choice)
 	
 	if valid_choice==0:
-		logger.info('Returning to menu, user choice was invalid')
+		logger.info('User choice was invalid')
 		# print(current_product_name)
-		logger.info('Fn: showProduct() exited')
 		return
 	
 	if not update_current_product():			#true if function was a success(i.e returned 0)
 		#get product name from db
-		logger.info('DB Query: selecting product name for product_id %d',current_product)
 		cursor.execute('''SELECT product_name FROM product_info_table WHERE product_id=?''',(current_product,))
-		logger.info('DB Query: SUCCESS')
-		
 		current_product_name=cursor.fetchone()[0]
 		all_suggestions.add(current_product)
 		# print(current_product_name)
@@ -91,13 +84,11 @@ def showProduct():
 		logger.info('Current product being set to None')
 		current_product=None
 		current_product_name=None
-	logger.info('Fn: showProduct() exited')
 
 def update_new_input():
 	""" Add the liked/disliked item in the user's like/dislike set """
 	global logger
 	global current_set_l, current_set_d, current_product, menu_val,new_set_l,new_set_d
-	logger.info('Fn: update_new_input() called with menu_val: %s',menu_val)
 	
 	if menu_val == 'y':
 		current_set_l.add(current_product)
@@ -108,36 +99,24 @@ def update_new_input():
 		new_set_d.add(current_product)
 		logger.info('%d added to dislike set of current user, dislike set size:%d',current_product,len(current_set_d))
 
-	logger.info('Fn: update_new_input() exited')
-
 def push_user_data_to_db():
 	"""	Push the new like/dislike data collected for the current user to the DB	"""
 	global logger
 	global db,cursor,user_id,new_set_l,new_set_d
-	logger.info('Fn: push_user_data_to_db() called')
 	
 	# if no data has been collected,exit 
 	if(len(new_set_l)+len(new_set_d)<1):
 		logger.info('size of user like dislike set is 0')
-		logger.info('Fn: push_user_data_to_db() exited')
 		return
 	else:
 		# iterate over each in new like and dislike set and add to the likes table
 		for elem in new_set_l:
-			logger.info('DB Query: Inserting product:%d in new like set of user:%d',elem,user_id)
 			cursor.execute('''INSERT INTO user_inputs_table(user_id,product_id,input_val) VALUES (?,?,1)''', (user_id,elem))
-			logger.info('DB Query: SUCCESS')				
-		
 		for elem in new_set_d:
-			logger.info('DB Query: Inserting product:%d in new dislike set of user:%d',elem,user_id)
 			cursor.execute('''INSERT INTO user_inputs_table(user_id,product_id,input_val) VALUES (?,?,-1)''', (user_id,elem))
-			logger.info('DB Query: SUCCESS')
-
-		logger.info('DB Commit: commiting to db')
 		db.commit()
-		logger.info('DB Commit: SUCCESS')
-	logger.info('Fn: push_user_data_to_db() exited')
-
+		logger.info('pushed user likes and dislikes data to db ')
+		
 def get_user_id():
 	"""
 	Brief: Get the unique user_id and the like & dislike set of the user
@@ -157,19 +136,13 @@ def get_user_id():
 	current_set_d=set()
 	new_set_d=set()
 
-	logger.info('Fn: get_user_id() called')
 	if user_persona == None:
-		logger.info('Checking if user exists')
-		
-		logger.info('DB Query: Select user id and persona for email : %s',user_email)
 		cursor.execute('''SELECT user_id,persona FROM user_info_table WHERE email_id=?''',(user_email,))
 		user_selection=cursor.fetchone()
-		logger.info('DB QUery: Success, selection is %s',repr(user_selection))
 		
 		if user_selection==None:
 			logger.info('User was not found, set user_id to None and exit')
 			user_id=None
-			logger.info('Fn: get_user_id() exited')
 			return 
 		else:
 			#user already present ,update user's like and dislike profile
@@ -178,58 +151,50 @@ def get_user_id():
 			logger.info('User found in db with id %d and persona:%s',user_id,user_persona)
 			
 			#like set
-			logger.info('DB Query: Selecting the like set for user %d',user_id)
 			cursor.execute('''SELECT product_id FROM user_inputs_table WHERE user_id=? AND input_val=1''',(user_id,))
-			logger.info('DB Query: SUCCESS')
 			current_set_l=set([i[0] for i in cursor.fetchall()])
 			logger.info('Like set for user %d is %s',user_id,repr(current_set_l))
 			
 			#dislike set
-			logger.info('DB Query: Selecting the dislike set for user %d',user_id)
 			cursor.execute('''SELECT product_id FROM user_inputs_table WHERE user_id=? AND input_val=-1''',(user_id,))
-			logger.info('DB Query: SUCCESS')
 			current_set_d=set([i[0] for i in cursor.fetchall()])
 			logger.info('DisLike set for user %d is %s',user_id,repr(current_set_d))
 
 	else:
 		logger.info('Adding new user to user info table')
-		
-		logger.info('DB Query : Insert user with email: %s and persona :%s',user_email,user_persona)
 		cursor.execute('''INSERT INTO user_info_table(email_id,persona) VALUES(?,?)''',(user_email,user_persona))
-		logger.info('DB Query: Success')
-		
-		logger.info('DB Query: Select the user id form the user info table')
 		cursor.execute('''SELECT user_id FROM user_info_table WHERE email_id=? AND persona=?''',(user_email,user_persona))
 		user_id=cursor.fetchone()[0]
 		logger.info('DB Query: Success , user id is %d',user_id)
-		
 		db.commit()
-		logger.info('DB Commit:Successful')
-	logger.info('Fn: get_user_id() exited')
-
+		
 def update_current_product():
 		
 	global logger
 	global current_product,n_users,n_users_dset,n_users_jset,n_users_lset,current_set_l,current_set_d
 	global n_products,n_pr_dset,n_pr_pval,n_pr_lset
-	global trending_set,fallback_set
-	logger.info('Fn: update_current_product() called')
+	global trending_set,fallback_set,model_value
 	
 	if len(n_users) <1 :
 		#show trending products
-		logger.info('No previous dataset of users,show trending products')
-		probable_set=set((trending_set - (current_set_d | current_set_l | all_suggestions)))
-		logger.info('Probable set is %s',repr(probable_set))
-		
-		if len(probable_set) <1:
+		current_product=pick_product(trending_set)
+		if current_product == None:
 			# print('Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
-			logger.info('Fn: update_current_product() exited with return value 1')
 			return 1
-		else:
-			current_product=random.sample(probable_set,1)[0]
+		else :
 			logger.info('current product value is %d',current_product)
-			logger.info('Fn: update_current_product() exited with return value 0')
 			return 0
+		# logger.info('No previous dataset of users,show trending products')
+		# probable_set=set((trending_set - (current_set_d | current_set_l | all_suggestions)))
+		# logger.info('Probable set is %s',repr(probable_set))
+		
+		# if len(probable_set) <1:
+			# # print('Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
+			# return 1
+		# else:
+			# current_product=random.sample(probable_set,1)[0]
+			# logger.info('current product value is %d',current_product)
+			# return 0
 	else:
 		#prev data is present, if user input is more than 2
 		if (len(current_set_d)+len(current_set_l)) > 2:
@@ -245,21 +210,25 @@ def update_current_product():
 				return model4()
 		else:
 			# show a random product to build user profile
-			# or show a product from the top/least number of inputs				
 			logger.info('NOW showing random product as user inputs were not enough')
-			probable_set=set((fallback_set - (current_set_d | current_set_l | all_suggestions)))
-			logger.info('Probable set for random product is %s',repr(probable_set))
-			if len(probable_set) <1:
+			current_product=pick_product(fallback_set)
+			if current_product == None:
 				print('2Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
-				logger.info('random probable set size was less than 1')
-				logger.info('Fn: update_current_product() exited with return value 1')
 				return 1
 			else:
-				current_product=random.sample(probable_set,1)[0]
 				logger.info('current_product is set to %d',current_product)
-				logger.info('Fn: update_current_product() exited with return value 0')
 				return 0
-	logger.info('Fn: update_current_product() exited with return value None')
+				
+			# probable_set=set((fallback_set - (current_set_d | current_set_l | all_suggestions)))
+			# logger.info('Probable set for random product is %s',repr(probable_set))
+			# if len(probable_set) <1:
+				# print('2Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
+				# logger.info('random probable set size was less than 1')
+				# return 1
+			# else:
+				# current_product=random.sample(probable_set,1)[0]
+				# logger.info('current_product is set to %d',current_product)
+				# return 0
 		
 def populate_previous_data():
 		""" 
@@ -268,7 +237,6 @@ def populate_previous_data():
 		global logger
 		global db,cursor,n_users_lset,n_users_dset,n_users_jset,n_users,trending_set,user_persona,fallback_set,user_id
 		global n_products,n_pr_dset,n_pr_pval,n_pr_lset,all_suggestions
-		logger.info('Fn: populate_previous_data() called')
 				
 		# users data
 		n_users_lset=dict()
@@ -287,9 +255,7 @@ def populate_previous_data():
 		#true_sample_set=set()
 		
 		#get list of previous products,trending and random sets
-		logger.info('DB Query: Selecting all previous products for persona:%s',user_persona)
 		cursor.execute('''SELECT product_id,trending FROM product_info_table WHERE persona=?''',(user_persona,))
-		logger.info('DB Query: SUCCESS')
 		n_products=[i[0] for i in cursor.fetchall()] # converting [(1,), (2,), (3,)] to [1, 2, 3]
 		# no products found exit the application , LOL 
 		if len(n_products)<1:
@@ -297,63 +263,40 @@ def populate_previous_data():
 			return 1
 
 		trending_set=set([i[0] for i in cursor.fetchall() if i[1]==1])
-		logger.info('Trending set LEN:%d and set is :%s',len(trending_set),repr(trending_set))
-		logger.info('Products are LEN:%d and list is :%s',len(n_products),repr(n_products))
-
-		#populate truesampleset
-		# true_sample_set=set(random.sample(trending_set | fallback_set,10))
 				
 		#iterate and populate product's like and dislike sets
-		logger.info('Starting population of products liked/disliked set')
 		for elem in n_products:
 			#populate like set
-			logger.info('DB Query: Fetching like set of product %d',elem)
 			cursor.execute('''SELECT user_id FROM user_inputs_table WHERE product_id=? AND input_val=1''',(elem,))
-			logger.info('DB Query: SUCCESS')
 			n_pr_lset[elem]=set([i[0] for i in cursor.fetchall()])
-			logger.info('Like set of product %d is of LEN:%d and set is :%s',elem,len(n_pr_lset[elem]),repr(n_pr_lset[elem]))
 			
 			#populate dislikes set
-			logger.info('DB Query: Fetching dislike set of product %d',elem)
 			cursor.execute('''SELECT user_id FROM user_inputs_table WHERE product_id=? AND input_val=-1''',(elem,))
-			logger.info('DB Query: SUCCESS')
 			n_pr_dset[elem]=set([i[0] for i in cursor.fetchall()])
-			logger.info('disLike set of product %d is of LEN:%d and set is :%s',elem,len(n_pr_dset[elem]),repr(n_pr_dset[elem]))
-			#initialize the similarity index value
+			#initialize the index value
 			n_pr_pval[elem]=None
 		
-
 		#get list of previous users
-		logger.info('DB Query: Selecting all previous users for persona:%s',user_persona)
 		cursor.execute('''SELECT user_id FROM user_info_table WHERE persona=?''',(user_persona,))
-		logger.info('DB Query: SUCCESS')
 		n_users=[i[0] for i in cursor.fetchall()] # converting [(1,), (2,), (3,)] to [1, 2, 3]
 		n_users.remove(user_id)
-		logger.info('Users are LEN:%d and list is :%s',len(n_users),repr(n_users))
-
+		
 		#if no users found, exit 
 		if len(n_users)<1:
 			# print('Looks like you are first in this category!!')
-			logger.info('No previous users')
 			logger.info('Fn: populate_previous_data() exited')
 			return 0
 		
 		#iterate and populate user's like and dislike sets
-		logger.info('Starting selection of all users like and dislike sets')
 		for elem in n_users:
 			#populate like set
-			logger.info('DB Query: Fetching like set of user %d',elem)
 			cursor.execute('''SELECT product_id FROM user_inputs_table WHERE user_id=? AND input_val=1''',(elem,))
-			logger.info('DB Query: SUCCESS')
 			n_users_lset[elem]=set([i[0] for i in cursor.fetchall()])
-			logger.info('Like set of user %d is of LEN:%d and set is :%s',elem,len(n_users_lset[elem]),repr(n_users_lset[elem]))
 			
 			#populate dislikes set
-			logger.info('DB Query: Fetching dislike set of user %d',elem)
 			cursor.execute('''SELECT product_id FROM user_inputs_table WHERE user_id=? AND input_val=-1''',(elem,))
-			logger.info('DB Query: SUCCESS')
 			n_users_dset[elem]=set([i[0] for i in cursor.fetchall()])
-			logger.info('disLike set of user %d is of LEN:%d and set is :%s',elem,len(n_users_dset[elem]),repr(n_users_dset[elem]))
+			
 			#initialize the similarity index value
 			n_users_jset[elem]=None
 		
@@ -364,7 +307,6 @@ def populate_previous_data():
 		fallback_set=set([i[0] for i in templist[0:30]])
 		logger.info('Fallback set LEN:%d and set is :%s',len(fallback_set),repr(fallback_set))
 
-		logger.info('Fn: populate_previous_data() exited')
 		return 0
 
 def print_personas():
@@ -387,7 +329,6 @@ def set_user(arg1,arg2):
 def set_user_input(arg1):
 	
 	global menu_val
-
 	if arg1 == 1:
 		menu_val='y';update_new_input()
 	elif arg1 == -1:
@@ -401,10 +342,8 @@ def getProduct():
 	return current_product
 
 def start(model_no=4):
-	global db,cursor,logger,handler
-
+	global db,cursor,logger,handler,model_value
 	#connect to db
-	logger.info('Connecting to train.db')
 	db=sqlite3.connect('train.db')
 	cursor=db.cursor()
 	logger.info('Connected to db')
@@ -421,6 +360,14 @@ def end():
 	logging.shutdown()
 
 ''' FUNCTION DEFINITIONS END'''
+
+def pick_product(set_name):
+	pset=set((set_name - (current_set_d | current_set_l | all_suggestions)))
+	if len(pset) <1:
+		return None
+	else:
+		return random.sample(pset,1)[0]
+
 
 # probability method
 def model1():
@@ -491,7 +438,7 @@ def model1():
 		return 0
 	return 1
 	
-# nearest user method
+# calc similarity with users, sort acc to similarity , pick random like from nearest user
 def model2():
 
 	"""
@@ -505,52 +452,51 @@ def model2():
 	
 	#the user has put in atleast 2 inputs till now
 	logger.info('user input is greater than 2. calculating similarity values')
+	
+	# calculate similarity values with every user
 	for elem in n_users:
+		
 		calc_val=0				
 		# jaccard index calculation
 		calc_val+= len(n_users_lset[elem] & current_set_l)
 		calc_val+= len(n_users_dset[elem] & current_set_d)
-		logger.info('The agreement total for user %d is %d',elem,calc_val)
-		
 		calc_val-= len(n_users_lset[elem] & current_set_d)
 		calc_val-= len(n_users_dset[elem] & current_set_l)
-		logger.info('The numerator value for user %d is %d',elem,calc_val)
-		
-		logger.info('The denominator value for user %d is %d',elem,(len(current_set_d)+len(current_set_l)))
 		calc_val/= (len(current_set_d)+len(current_set_l))
-		logger.info('The similarity value for user %d is %.4f',elem,calc_val)
 		n_users_jset[elem]=calc_val;
 	
-					
-	# for all nearest neighbours in descending order of similarity index
+	# sort users in descending order of similarity index
 	nearest_order=sorted(n_users_jset, key=n_users_jset.get,reverse=True)
 	logger.info('Nearest users order is %s',repr(nearest_order))
 	for elem in nearest_order:
 		# try their like set
-		probable_set=set((n_users_lset[elem] - (current_set_d | current_set_l | all_suggestions)))
-		logger.info('Probable set with user %d is %s',elem,repr(probable_set))
-		if len(probable_set)>0:
-			current_product=random.sample(probable_set,1)[0] #choose any one from nearest user's like set
-			logger.info('current_product is set to %d',current_product)
-			logger.info('Fn: update_current_product() exited with return value 0')
+		current_product=pick_product(n_users_lset[elem])
+		if current_product != None:
 			return 0
+		# probable_set=set((n_users_lset[elem] - (current_set_d | current_set_l | all_suggestions)))
+		# logger.info('Probable set with user %d is %s',elem,repr(probable_set))
+		# if len(probable_set)>0:
+			# current_product=random.sample(probable_set,1)[0] #choose any one from nearest user's like set
+			# logger.info('current_product is set to %d',current_product)
+			# return 0
 	
-	#if reached here, means in loop anywhere we could not find a product,show user more products from the fallback set 
-	# show a fallback product to build better user profile
-	logger.info('NOW showing fallback product, no user product matched')
-	probable_set=set((fallback_set - (current_set_d | current_set_l | all_suggestions)))
-	logger.info('Probable set for fallback product is %s',repr(probable_set))
-	if len(probable_set) <1:
+	#if reached here, we could not find a product,show user more products from the fallback set 
+	current_product=pick_product(fallback_set)
+	if current_product == None:
 		print('1Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
-		logger.info('fallback probable set size was less than 1')
-		logger.info('Fn: update_current_product() exited with return value 1')
 		return 1
 	else:
-		current_product=random.sample(probable_set,1)[0]
 		logger.info('current_product is set to %d',current_product)
-		logger.info('Fn: update_current_product() exited with return value 0')
 		return 0
-	''' OLD '''
+	# logger.info('NOW showing fallback product, no user product matched')
+	# probable_set=set((fallback_set - (current_set_d | current_set_l | all_suggestions)))
+	# if len(probable_set) <1:
+		# print('1Phew!! we are all exhausted here, thank you for your inputs. See you next time.')
+		# return 1
+	# else:
+		# current_product=random.sample(probable_set,1)[0]
+		# logger.info('current_product is set to %d',current_product)
+		# return 0
 	
 #item-item similarity
 def model3():
